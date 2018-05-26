6342e953283a457b5e93e8d64b44d1118fdb5e67
santosh <santosh.ece06@gmail.com>
2018-05-26 15:59:32 +0530

    angular bootsrtrap mateirla integration.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, NO_ERRORS_SCHEMA } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { MDBBootstrapModule } from 'angular-bootstrap-md';
+import {CustomModule} from './custom/custom.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    MDBBootstrapModule.forRoot(),
+    BrowserModule,
+    BrowserAnimationsModule,
+    CustomModule
   ],
+  schemas: [ NO_ERRORS_SCHEMA ],
   providers: [],
   bootstrap: [AppComponent]
 })
